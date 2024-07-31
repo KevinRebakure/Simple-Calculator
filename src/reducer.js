4556e295b6da4b47eea65cf1c5281f824db7cbe1
Rebakure <kevinrebakure@gmail.com>
2024-07-31 12:42:00 +0200

    Some bugs
    1. Typing many unnecessary zeros
    2. Pressing equal sign 2 times also lead to problems. It's like the whole things restart

--- src/reducer.js
+++ src/reducer.js
@@ -72,53 +72,55 @@ export default function reducer(state, action) {
         current: "",
       };
     case ACTIONS.ENTER:
-      switch (state.operation) {
-        case "+":
-          return {
-            previous: (
-              parseFloat(state.previous) + parseFloat(state.current)
-            ).toString(),
-            operation: "",
-            current: "",
-            display: (
-              parseFloat(state.previous) + parseFloat(state.current)
-            ).toString(),
-          };
-        case "-":
-          return {
-            previous: (
-              parseFloat(state.previous) - parseFloat(state.current)
-            ).toString(),
-            operation: "",
-            current: "",
-            display: (
-              parseFloat(state.previous) - parseFloat(state.current)
-            ).toString(),
-          };
-        case "/":
-          return {
-            previous: (
-              parseFloat(state.previous) / parseFloat(state.current)
-            ).toString(),
-            operation: "",
-            current: "",
-            display: (
-              parseFloat(state.previous) / parseFloat(state.current)
-            ).toString(),
-          };
-        case "*":
-          return {
-            previous: (
-              parseFloat(state.previous) * parseFloat(state.current)
-            ).toString(),
-            operation: "",
-            current: "",
-            display: (
-              parseFloat(state.previous) * parseFloat(state.current)
-            ).toString(),
-          };
-        default:
-          return { state };
+      if (state.current && state.previous && state.operation) {
+        switch (state.operation) {
+          case "+":
+            return {
+              previous: (
+                parseFloat(state.previous) + parseFloat(state.current)
+              ).toString(),
+              operation: "",
+              current: "",
+              display: (
+                parseFloat(state.previous) + parseFloat(state.current)
+              ).toString(),
+            };
+          case "-":
+            return {
+              previous: (
+                parseFloat(state.previous) - parseFloat(state.current)
+              ).toString(),
+              operation: "",
+              current: "",
+              display: (
+                parseFloat(state.previous) - parseFloat(state.current)
+              ).toString(),
+            };
+          case "/":
+            return {
+              previous: (
+                parseFloat(state.previous) / parseFloat(state.current)
+              ).toString(),
+              operation: "",
+              current: "",
+              display: (
+                parseFloat(state.previous) / parseFloat(state.current)
+              ).toString(),
+            };
+          case "*":
+            return {
+              previous: (
+                parseFloat(state.previous) * parseFloat(state.current)
+              ).toString(),
+              operation: "",
+              current: "",
+              display: (
+                parseFloat(state.previous) * parseFloat(state.current)
+              ).toString(),
+            };
+          default:
+            return { ...state };
+        }
       }
     case ACTIONS.CLEAR:
       return { previous: "", current: "", operation: "", display: "" };
